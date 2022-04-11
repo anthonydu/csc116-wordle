8091b844e10db53bb8b8a33c8b86dc4bf520b633
Anthony Du <pdu3@ncsu.edu>
2022-04-11 11:49:02 -0400

    Updated Wordle Class

--- src/Wordle.java
+++ src/Wordle.java
@@ -19,11 +19,12 @@ public class Wordle {
     public static void main(String[] args) {
 
         String[] valids = getWords("txt/valid_words.txt");
-        String[] possibles = getWords("txt/possible_words.txt");
+        String[] answers = getWords("txt/answer_words.txt");
 
-        String answer = possibles[new Random().nextInt(possibles.length)];
+        String answer = answers[new Random().nextInt(answers.length)];
 
-        String[] guesses = new String[]{"first", "second", "third", "forth", "fifth", "sixth and final"};
+        String[] guesses = new String[]{"first", "second", "third",
+                                        "forth", "fifth", "sixth and final"};
 
         Scanner console = new Scanner(System.in);
 
@@ -31,41 +32,47 @@ public class Wordle {
 
         for (int i = 0; i < 6; i++) {
 
-            while (true) {
+            while (true) { // keep prompting until user gives an input
                 System.out.print("Please enter your " + guesses[i] + " guess: ");
                 if (console.hasNext()) break;
             }
-            String guess = console.next().toLowerCase();
+            String guess = console.next().toLowerCase(); // convert to all lower case
 
-            if (guess.equals("q")) {
-                System.out.print("Thanks for playing!");
+            if (guess.equals("q")) { // quit if the input is "q"
+                System.out.println();
+                System.out.println("Thanks for playing!");
+                System.out.println("The word was " + answer.toUpperCase() + ".");
+                System.out.println();
                 System.exit(0);
-            } else if (guess.length() != 5) {
+            } else if (guess.length() != 5) { // if guess is not a five-letter word
                 System.out.println("Please enter a five-letter word.");
-                i -= 1;
+                i -= 1; // reset the loop with the same i value
                 continue;
-            } else if (!isStringInArray(guess, valids)) {
+            } else if (!isStringInArray(guess, valids)) { // if guess is not in the valid word list
                 System.out.println(guess.toUpperCase() + " is not a valid word.");
-                i -= 1;
+                i -= 1; // reset the loop with the same i value
                 continue;
             }
 
             String indicators = generateIndicators(guess, answer);
 
-            if (indicators.equals("VVVVV")) {
+            if (indicators.equals("VVVVV")) { // they got the word
                 System.out.println();
                 System.out.println("You won! The word was " + answer.toUpperCase() + ".");
-                System.out.println("You got the answer in " + (i + 1) + " guesses!");
+                if (i == 0) {
+                    System.out.println("You got the answer in 1 guess?!");
+                } else {
+                    System.out.println("You got the answer in " + (i + 1) + " guesses!");
+                }
                 System.out.println();
-                System.exit(1);
-            } else if (i != 5) {
+            } else if (i != 5) { // else, only print indicators if it's not the last guess
                 System.out.println();
                 System.out.println("\t" + guess.toUpperCase());
                 System.out.println("\t" + indicators);
                 System.out.println();
             }
         }
-
+        // The for loop ran out. Player lost the game.
         System.out.println();
         System.out.println("You did not guess the correct word.");
         System.out.println("The correct word was " + answer.toUpperCase() + ".");
@@ -91,6 +98,9 @@ public class Wordle {
     }
 
     private static String generateIndicators(String guess, String answer) {
+
+        // This generation algorithem should give the exact same results as the original Wordle.
+
         if (guess.length() != answer.length()) {
             throw new IllegalArgumentException("Different lengths!");
         }
@@ -142,17 +152,16 @@ public class Wordle {
         System.out.println("Hit the enter button to submit your guess.");
         System.out.println("Type in \"q\" and submit to leave the game.");
         System.out.println();
-        System.out.println("After each guess, you will be shown how close your guess was to the word:");
+        System.out.println("After each guess, "
+                         + "you will be shown how close your guess was to the word:");
         System.out.println();
-        System.out.println("\tV - letter in the correct spot");
-        System.out.println("\tO - letter in the word but wrong spot");
-        System.out.println("\tX - letter not in the word");
+        System.out.println("V - letter is in the correct spot");
+        System.out.println("O - letter is in the word but wrong spot");
+        System.out.println("X - letter is not in the word");
         System.out.println();
     }
 
     private static String removeLetter(String s, char c) {
-        s = s.substring(0, s.indexOf(c)) + s.substring(s.indexOf(c) + 1);
-        return s;
+        return s.substring(0, s.indexOf(c)) + s.substring(s.indexOf(c) + 1);
     }
-
 }
